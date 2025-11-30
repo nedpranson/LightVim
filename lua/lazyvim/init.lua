6b3c6a999e1dd33bcf40ca6e78f86f3300b5ff18
Nedas Pranskunas <nedas.pranskunas@gmail.com>
2025-11-30 22:26:40 +0200

    Lua fmt.

--- lua/lazyvim/init.lua
+++ lua/lazyvim/init.lua
@@ -5,7 +5,7 @@ M.opts = {}
 function M.setup(opts)
   local LazyVim = require("lazy.util")
 
-	M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+  M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
 
   LazyVim.try(function()
     if type(M.opts.colorscheme) == "function" then
@@ -14,12 +14,12 @@ function M.setup(opts)
       vim.cmd.colorscheme(M.opts.colorscheme)
     end
   end, {
-    msg = "Could not load your colorscheme",
-    on_error = function(msg)
-      LazyVim.error(msg)
-      vim.cmd.colorscheme("habamax")
-    end,
-  })
+  msg = "Could not load your colorscheme",
+  on_error = function(msg)
+    LazyVim.error(msg)
+    vim.cmd.colorscheme("habamax")
+  end,
+})
 end
 
 return M
